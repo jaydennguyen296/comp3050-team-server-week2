41e1961b78541f86e7acdc29e7de0cb54b604119
stalebiscuit <161665600+stalebiscuit@users.noreply.github.com>
2026-03-04 16:27:34 +1100

    Week 1 starter files (corrected cors and json)

---
+++ MyHandler.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.management.VMOption.Origin;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class MyHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+
+        // Handle CORS.
+        String origin = he.getRequestHeaders().getFirst("Origin");
+        he.getResponseHeaders().add("Access-Control-Allow-Origin", origin);
+        he.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        he.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+        // Set response headers.
+        Headers headers = he.getResponseHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Connection", "close");
+
+        // Send response body.
+        String json = "{ \"name\": \"Daniel\", \"gold\": 3, \"silver\": 2, \"bronze\": 1}";
+        //String response = "This is the response for server 2";
+        System.out.println(json);
+        he.sendResponseHeaders(200, json.length());
+        OutputStream os = he.getResponseBody();
+        os.write(json.getBytes());
+        os.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Simple server
+
+This is a simple Java web server with corresponding HTML script.
+
+The simple_server2 folder contains a Java web server.
+* Compile with javac Test.java
+* Run with java Test
+* Check it's working by visiting http://localhost:8000/test
+
+The simple_script2.html file has a web form that talks to the Java server.
+
+Can you get it working on your own laptop?
+
+Yes it shows a response in the terminal
+
+# Questions
+
+Examine version 1 of the Java code, in the simple_server1 folder.
+What was added to simple_server1 to make version 2? Why?
+
+getResponseHeaders were added which specifies a content type and connection to allow the server to connect to a html website. Therefore allowing a response from the server when the send button is clicked.
+
+What does commenting out some of the added parts of simple_server2 do?
+Does it still work, and why or why not?
+
+
+# Task
+
+Make a copy of these files and modify them to send JSON data instead.
+
+What can you send?
+
+# Portfolio
+
+Remember to keep these new files and any notes you've made.
+These will be useful for your practical portfolio report.
+Make your notes a useful learning resource for someone reading them later.
